98036eb8965597edde611dbbb0b4d8113ccaa685
KINDEL Hugo <hugokindel.pro@protonmail.com>
2021-02-08 15:53:30 +0100

    add event system

---
+++ src/main/java/com/ustudents/farmland/event/EventDispatcher.java
@@ -0,0 +1,22 @@
+package com.ustudents.farmland.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventDispatcher {
+    List<EventListener> listeners;
+
+    public EventDispatcher() {
+        listeners = new ArrayList<>();
+    }
+
+    public void dispatch() {
+        for (EventListener listener : listeners) {
+            listener.onReceived();
+        }
+    }
+
+    public void add(EventListener listener) {
+        listeners.add(listener);
+    }
+}
