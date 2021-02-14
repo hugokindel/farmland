4b7ea84a4b1973d21a37d79c59570d0f503d7292
LE CORRE leo <karmaessence@outlook.fr>
2021-02-14 21:54:13 +0100

    fix bug

--- src/main/java/com/ustudents/engine/core/Resources.java
+++ src/main/java/com/ustudents/engine/core/Resources.java
@@ -2,6 +2,7 @@ package com.ustudents.engine.core;
 
 import com.ustudents.engine.Game;
 import com.ustudents.engine.core.cli.print.Out;
+import com.ustudents.engine.graphic.Font;
 import com.ustudents.engine.graphic.Shader;
 import com.ustudents.engine.graphic.Texture;
 import com.ustudents.engine.core.json.JsonReader;
@@ -23,6 +24,7 @@ public class Resources {
     private static final String shadersDirectoryName = "shaders";
     private static final String texturesDirectoryName = "textures";
     private static final String playersDirectoryName = "players";
+    private static final String fontsDirectoryName = "fonts";
     private static final String settingsFilename = "settings.json";
     private static final ReentrantReadWriteLock settingsLock = new ReentrantReadWriteLock();
     private static final Lock settingsReadLock = settingsLock.readLock();
@@ -30,6 +32,7 @@ public class Resources {
     private static Map<String, Object> settings;
     private static Map<String, Shader> shaders;
     private static Map<String, Texture> textures;
+    private static Map<String, Map<Integer, Font>> fonts;
 
     /**
      * Gets the data directory's path.
@@ -55,6 +58,10 @@ public class Resources {
 
     public static String getTexturesDirectory() {
         return createPathIfNeeded(getDataDirectory() + "/" + texturesDirectoryName);
+    }
+
+    public static String getFontsDirectory() {
+        return createPathIfNeeded(getDataDirectory() + "/" + fontsDirectoryName);
     }
 
     public static String getPlayersDirectoryName(){
@@ -254,6 +261,55 @@ public class Resources {
 
             if (removeFromList) {
                 textures.remove(filePath);
+            }
+        }
+    }
+
+    public static Font loadFont(String filePath, int fontSize) {
+        if (!fonts.containsKey(filePath)) {
+            fonts.put(filePath, new HashMap<>());
+        }
+
+        if (!fonts.get(filePath).containsKey(fontSize)) {
+            if (Game.isDebugging()) {
+                Out.printlnDebug("Font loaded: " + getFontsDirectory() + "/" + filePath + "");
+            }
+
+            fonts.get(filePath).put(fontSize, new Font(getFontsDirectory() + "/" + filePath, fontSize));
+        }
+
+        Font font = fonts.get(filePath).get(fontSize);
+
+        if (font.isDestroyed()) {
+            fonts.remove(filePath);
+            return loadFont(filePath, fontSize);
+        }
+
+        return font;
+    }
+
+    public static Font getFont(String filePath, int fontSize) {
+        return fonts.get(filePath).get(fontSize);
+    }
+
+    public static void unloadFont(String filePath, int fontSize) {
+        unloadFont(filePath, fontSize, true);
+    }
+
+    static void unloadFont(String filePath, int fontSize, boolean removeFromList) {
+        if (fonts.containsKey(filePath) && fonts.get(filePath).containsKey(fontSize)) {
+            if (Game.isDebugging()) {
+                Out.printlnDebug("Font unloaded: " + getFontsDirectory() + "/" + filePath + "");
+            }
+
+            fonts.get(filePath).get(fontSize).destroy();
+
+            if (removeFromList) {
+                fonts.get(filePath).remove(fontSize);
+
+                if (fonts.get(filePath).isEmpty()) {
+                    fonts.remove(filePath);
+                }
             }
         }
     }
