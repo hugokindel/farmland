a400c6db87c0e535563a6b8c3ee99772fa92831d
LE CORRE leo <karmaessence@outlook.fr>
2021-02-17 23:17:47 +0100

    Add customizable mouse button

---
+++ src/main/java/com/ustudents/engine/input/Mouse.java
@@ -0,0 +1,18 @@
+package com.ustudents.engine.input;
+
+import org.lwjgl.glfw.*;
+
+public class Mouse {
+    public static final int Button_1 = GLFW.GLFW_MOUSE_BUTTON_1;
+    public static final int Button_2 = GLFW.GLFW_MOUSE_BUTTON_2;
+    public static final int Button_3 = GLFW.GLFW_MOUSE_BUTTON_3;
+    public static final int Button_4 = GLFW.GLFW_MOUSE_BUTTON_4;
+    public static final int Button_5 = GLFW.GLFW_MOUSE_BUTTON_5;
+    public static final int Button_6 = GLFW.GLFW_MOUSE_BUTTON_6;
+    public static final int Button_7 = GLFW.GLFW_MOUSE_BUTTON_7;
+    public static final int Button_8 = GLFW.GLFW_MOUSE_BUTTON_8;
+    public static final int Button_Last = GLFW.GLFW_MOUSE_BUTTON_LAST;
+    public static final int Button_Left = GLFW.GLFW_MOUSE_BUTTON_LEFT;
+    public static final int Button_Right = GLFW.GLFW_MOUSE_BUTTON_RIGHT;
+    public static final int Button_Middle = GLFW.GLFW_MOUSE_BUTTON_MIDDLE;
+}
