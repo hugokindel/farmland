f544ad83f5eb0b43dc700d3a82330b890c57d03b
JAUROYON maxime <maxime.jauroyon@gmail.com>
2021-02-24 17:11:31 +0100

    Rebuild the Data Base

--- src/main/java/com/ustudents/farmland/core/item/Animal.java
+++ src/main/java/com/ustudents/farmland/core/item/Animal.java
@@ -1,4 +1,6 @@
 package com.ustudents.farmland.core.item;
 
 public class Animal extends Item {
+
+    public Integer numberOfTurnsToReachMaturity;
 }
--- src/main/java/com/ustudents/farmland/core/item/Property.java
+++ src/main/java/com/ustudents/farmland/core/item/Property.java
@@ -1,4 +1,12 @@
 package com.ustudents.farmland.core.item;
 
 public class Property extends Item {
+
+    public Integer maintenanceCost;
+
+    public Integer initLevel;
+
+    public Integer actualLevel;
+
+    public Integer maxLevel;
 }
